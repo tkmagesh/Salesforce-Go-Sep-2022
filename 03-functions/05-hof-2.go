059aac6486e2deb40ff2990485f78cb0ecc9edc5
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-27 04:45:54 +0530

    Higher order functions (functions as arguments) EXAMPLES

--- 03-functions/05-hof-2.go
+++ 03-functions/05-hof-2.go
@@ -2,7 +2,12 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Operation func(int, int)
 
 func main() {
 	/*
@@ -10,22 +15,48 @@ func main() {
 		subtract(100,200)
 	*/
 
-	logAdd := getLoggedOperation(add)
-	logAdd(100, 200)
+	/*
+		logAdd := getLoggedOperation(add)
+		logAdd(100, 200)
+	*/
 
 	//logOperation(100, 200, add)
 
-	logSubtract := getLoggedOperation(subtract)
-	logSubtract(100, 200)
+	/*
+		logSubtract := getLoggedOperation(subtract)
+		logSubtract(100, 200)
+	*/
 
 	//logOperation(100, 200, subtract)
+
+	/* profiledAdd := getProfiledOperation(add)
+	profiledAdd(100, 200)
+
+	profiledSubtract := getProfiledOperation(subtract)
+	profiledSubtract(100, 200) */
+
+	add := getProfiledOperation(getLoggedOperation(add))
+	subtract := getProfiledOperation(getLoggedOperation(subtract))
+
+	add(100, 200)
+	subtract(100, 200)
 }
 
-func getLoggedOperation(operation func(int, int)) func(int, int) {
+//func getLoggedOperation(operation func(int, int)) func(int, int) {
+func getLoggedOperation(operation Operation) Operation {
 	return func(x, y int) {
 		fmt.Println("Invocation started")
 		operation(x, y)
 		fmt.Println("Invocation completed")
+	}
+}
+
+func getProfiledOperation(operation Operation) Operation {
+	return func(x, y int) {
+		start := time.Now()
+		operation(x, y)
+		elapsed := time.Now().Sub(start)
+		fmt.Println("Elapsed =", elapsed)
 	}
 }
 
