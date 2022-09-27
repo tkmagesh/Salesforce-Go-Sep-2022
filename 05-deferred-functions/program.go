05d43350117ce02a5a92718fa1f883294ff9b850
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-27 22:36:25 +0530

    panic & recovery

--- 05-deferred-functions/program.go
+++ 05-deferred-functions/program.go
@@ -31,14 +31,18 @@ func f1() {
 	defer fmt.Println("	deferred @ f1 [2] ")
 	defer fmt.Println("	deferred @ f1 [3] ")
 	fmt.Println("f1 started")
-	f2()
+	result := f2()
+	fmt.Println("Result from f2 = ", result)
 	fmt.Println("f1 completed")
 }
 
-func f2() {
+func f2() (result int) {
 	defer func() {
-		fmt.Println("	deferred @ f2")
+		fmt.Println("	deferred @ f2, chaning the result of f2 ")
+		result = 200
 	}()
 	fmt.Println("f2 started")
 	fmt.Println("f2 completed")
+	result = 100
+	return
 }
---
+++ 06-panic-recovery/program.go
@@ -0,0 +1,46 @@
+/* panic & recovery */
+
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var DivideByZeroError = errors.New("divisor cannot be 0")
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Something went wrong. ")
+			fmt.Println(err)
+		}
+		fmt.Println("Thank you")
+	}()
+	quotient, remainder, err := divideClient(100, 0)
+	if err == DivideByZeroError {
+		fmt.Println("Please do not divide by zero")
+	} else {
+		fmt.Println(quotient, remainder)
+	}
+}
+
+func divideClient(x, y int) (quotient, remainder int, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = e.(error)
+			return
+		}
+	}()
+	quotient, remainder = divide(x, y)
+	return
+}
+
+//3rd party library
+func divide(x, y int) (quotient, remainder int) {
+	if y == 0 {
+		panic(DivideByZeroError)
+	}
+	quotient, remainder = x/y, x%y
+	return
+}
