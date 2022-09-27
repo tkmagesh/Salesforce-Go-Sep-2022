1a22c460c09900975fb0514eb39525b02024fcce
Magesh Kuppan <tkmagesh77@gmail.com>
2022-09-28 02:14:34 +0530

    assignment 02 & 03 solution

--- 07-collections/assignment-02.go
+++ 07-collections/assignment-02.go
@@ -6,4 +6,33 @@ refactor the program to encapsulate the prime number generation logic in a funct
 the main function should be responsible for accpeting inputs and printing the result
 */
 
+package main
 
+import "fmt"
+
+func main() {
+	var start, end int
+	fmt.Println("Enter start and end :")
+	fmt.Scanln(&start, &end)
+	primeNos := generatePrimes(start, end)
+	fmt.Println(primeNos)
+}
+
+func generatePrimes(start, end int) []int {
+	primeNos := make([]int, 0)
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			primeNos = append(primeNos, no)
+		}
+	}
+	return primeNos
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
